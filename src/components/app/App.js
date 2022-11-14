5806307c864a7e39001bf3c50471f696e3e097db
Wu2ko <98595248+Wu2ko@users.noreply.github.com>
2022-11-14 15:07:35 +0200

    Add files via upload
    npm i
    npm i bootstrap

---
+++ src/components/app/App.js
@@ -0,0 +1,24 @@
+import Header from "../header/header";
+import Footer from "../footer/footer";
+import Main from "../main/main";
+import Nav from "../../nav/nav";
+
+function App() {
+    return (
+       <>
+            <Nav/>
+            <Header/>
+            <Main/>
+            <Footer/>
+            {/*taip atrodo komentaras -- reikia {}*/}
+        </>
+
+
+
+
+    );
+}
+
+export default App;
+
+
